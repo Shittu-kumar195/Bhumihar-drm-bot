aad18e8a0cbcf7708c15f7be01bdd0888fe3fac6
Shittu-kumar195 <southstatus31@gmail.com>
2025-03-04 00:22:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     VIP_USER = os.environ.get('VIP_USERS', '').split(',')
-    VIP_USERS = [int(user_id) for user_id in VIP_USER]
+    VIP_USERS = [int(8172163893) for user_id in VIP_USER]
